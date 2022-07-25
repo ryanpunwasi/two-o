9f30385d27c6cae5aacf10d58aa1953b247fcebd
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-07-25 19:52:54 -0400

    Add question string instance variable

--- question.rb
+++ question.rb
@@ -1,10 +1,12 @@
 class Question
   attr_reader :addend
   attr_reader :sum
+  attr_reader :question_string
 
   def initialize
     @addend = [1 + rand(20), 1 + rand(20)]
     @sum = @addend[0] + @addend[1]
+    @question_string = "What does #{@addend[0]} plus #{@addend[1]} equal?"
   end
 
 end
